4e87c21fc561e7bac3c63621abcbd041a38bc5cc
gabenasci <gabenascimento1@gmail.com>
2022-03-30 02:52:38 -0300

    On press styles for goal items

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,33 +1,41 @@
-import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
-  
   // function deleteGoalHandler() {
   //   props.onDeleteItem(props.id)
   // }
   // instead of declaring the above function, and changing the onPress function
   // to deleteGoalHandler - we can pass the function onDeleteItem argument (id)
-  // via .bind -> .bind() first parameter is this, and second parameter is the argument 
+  // via .bind -> .bind() first parameter is this, and second parameter is the argument
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>  
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#0f0064" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        // style prop below to add pressing effects on iOS
+        style={({ pressed }) => pressed && styles.pressedItem } // Obj destructuring of pressData
+        //style={(pressData) => pressData.pressed && styles.pressedItem }
+      >
         <Text style={styles.goalText}> {props.text} </Text>
-      </View>
-    </Pressable>
-  )
-};
+      </Pressable>
+    </View>
+  );
+}
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 6,
-    padding: 8,
     backgroundColor: "#2200e2",
     borderRadius: 6,
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
-    color: "white", // STYLES ARE NOT INHERITED,
-    //so we create style just for text
+    padding: 8,
+    color: "white", // STYLES IN RN ARE NOT INHERITED/CASCADING LIKE CSS,
+    //so we must have color: white just for goalText, and not in a parent component
   },
 });
